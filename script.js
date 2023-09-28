efaf7307226d668c5d05204cd1b33a97db22626f
Tyler Hoang <104310608+Website-Error404@users.noreply.github.com>
2023-09-28 23:04:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,33 +14,3 @@ document.getElementById('generate-key').addEventListener('click', function () {
     const randomKey = generateRandomKey(Math.floor(Math.random() * 5) + 6); // Random length between 6 and 10
     document.getElementById('key-display').innerText = randomKey;
 });
-
-// Check if the entered key is correct
-document.getElementById('check-key').addEventListener('click', function () {
-    const userKey = document.getElementById('user-key').value;
-    const generatedKey = document.getElementById('key-display').innerText;
-    if (userKey === generatedKey) {
-        document.getElementById('message').innerText = 'Key is correct!';
-    } else {
-        document.getElementById('message').innerText = 'Key is incorrect. Please try again.';
-    }
-});
-
-// Function to get random text
-function getRandomText() {
-    const texts = [
-        'Hello, world!',
-        'How are you?',
-        'What is your name?',
-        'Welcome to our website.',
-        'Have a great day!',
-    ];
-    const randomIndex = Math.floor(Math.random() * texts.length);
-    return texts[randomIndex];
-}
-
-// Display random text
-document.getElementById('get-random-text').addEventListener('click', function () {
-    const randomText = getRandomText();
-    document.getElementById('random-text').innerText = randomText;
-});
