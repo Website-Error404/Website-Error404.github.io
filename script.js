f52c5340b79cd2969cc07e4c1cfc18d36b93c8ca
Tyler Hoang <104310608+Website-Error404@users.noreply.github.com>
2023-09-28 23:16:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,40 +9,11 @@ function generateRandomKey(length) {
     return key;
 }
 
-// Generate and display a random key initially
-const initialRandomKey = generateRandomKey(Math.floor(Math.random() * 5) + 6); // Random length between 6 and 10
-document.getElementById('key-display').innerText = initialRandomKey;
+// Generate and display a random key
+document.getElementById('generate-key').addEventListener('click', function () {
+    const randomKey = generateRandomKey(Math.floor(Math.random() * 5) + 6); // Random length between 6 and 10
+    document.getElementById('key-display').innerText = randomKey;
 
-// Function to load and display random text from a file
-function loadRandomText() {
-    const textFiles = ['text1.txt', 'text2.txt', 'text3.txt', 'text4.txt', 'text5.txt']; // Add your file names here
-    const randomIndex = Math.floor(Math.random() * textFiles.length);
-    const chosenFile = textFiles[randomIndex];
-
-    // Assuming you're using a web server, you can fetch the text from the file like this:
-    fetch(chosenFile)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('random-text').innerText = data;
-        })
-        .catch(error => {
-            console.error('Error loading text:', error);
-        });
-}
-
-// Check if the entered key is correct
-document.getElementById('check-key').addEventListener('click', function () {
-    const userKey = document.getElementById('user-key').value;
-    const generatedKey = document.getElementById('key-display').innerText;
-    if (userKey === generatedKey) {
-        document.getElementById('message').innerText = 'Key is correct!';
-
-        // Load and display random text
-        loadRandomText();
-
-        // Hide the "Get Random Text" button
-        document.getElementById('get-random-text').style.display = 'none';
-    } else {
-        document.getElementById('message').innerText = 'Key is incorrect. Please try again.';
-    }
+    // Show the button to proceed to the check key page
+    document.getElementById('proceed-button').style.display = 'block';
 });
