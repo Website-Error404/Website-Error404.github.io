932d72fb176b71cc18016d10b16c1a8564ac8c0e
Tyler Hoang <104310608+Website-Error404@users.noreply.github.com>
2023-09-28 23:13:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,30 +13,36 @@ function generateRandomKey(length) {
 const initialRandomKey = generateRandomKey(Math.floor(Math.random() * 5) + 6); // Random length between 6 and 10
 document.getElementById('key-display').innerText = initialRandomKey;
 
+// Function to load and display random text from a file
+function loadRandomText() {
+    const textFiles = ['text1.txt', 'text2.txt', 'text3.txt', 'text4.txt', 'text5.txt']; // Add your file names here
+    const randomIndex = Math.floor(Math.random() * textFiles.length);
+    const chosenFile = textFiles[randomIndex];
+
+    // Assuming you're using a web server, you can fetch the text from the file like this:
+    fetch(chosenFile)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('random-text').innerText = data;
+        })
+        .catch(error => {
+            console.error('Error loading text:', error);
+        });
+}
+
 // Check if the entered key is correct
 document.getElementById('check-key').addEventListener('click', function () {
     const userKey = document.getElementById('user-key').value;
     const generatedKey = document.getElementById('key-display').innerText;
     if (userKey === generatedKey) {
         document.getElementById('message').innerText = 'Key is correct!';
-        
-        // Show random text
-        const randomText = getRandomText();
-        document.getElementById('random-text').innerText = randomText;
+
+        // Load and display random text
+        loadRandomText();
+
+        // Hide the "Get Random Text" button
+        document.getElementById('get-random-text').style.display = 'none';
     } else {
         document.getElementById('message').innerText = 'Key is incorrect. Please try again.';
     }
 });
-
-// Function to get random text
-function getRandomText() {
-    const texts = [
-        'Hello, world!',
-        'How are you?',
-        'What is your name?',
-        'Welcome to our website.',
-        'Have a great day!',
-    ];
-    const randomIndex = Math.floor(Math.random() * texts.length);
-    return texts[randomIndex];
-}
